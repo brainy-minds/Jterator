4b96689e97ff7817e82a3a5aa349619f9b8215f1
Markus Herrmann <markus.herrmann@imls.uzh.ch>
2015-01-25 23:11:33 +0100

    Let's try to send out a job for jterator 	modified:   src/python/brutus.py

--- src/python/brutus.py
+++ src/python/brutus.py
@@ -5,24 +5,12 @@ import re
 from subprocess32 import (PIPE, Popen, call)
 
 '''
-Script to run Jterator in parallel on Brutus.
+Script to run a Jterator pipeline in parallel mode on Brutus.
 
 Call this script from your project folder!
 '''
 
-
-# 1) Check pipeline
-process = Popen(['jt', 'check'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-
-(stdoutdata, stderrdata) = process.communicate()
-print stdoutdata
-print stderrdata
-
-if process.returncode > 0 or re.search('Failed', stderrdata):
-    raise Exception("""Pipeline check failed.\n
-Reason: \'%s\'""" % str(stderrdata))
-
-# 2) Create joblist
+# 1) Create joblist
 process = Popen(['jt', 'joblist'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
 (stdoutdata, stderrdata) = process.communicate()
@@ -33,26 +21,29 @@ if process.returncode > 0 or re.search('Failed', stderrdata):
     raise Exception("""Building joblist failed.\n
 Reason: \'%s\'""" % str(stderrdata))
 
-# 3) Run 'PreCluster'
-process = Popen(['jt', 'run', '--job', '1'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-
-(stdoutdata, stderrdata) = process.communicate()
-print stdoutdata
-print stderrdata
-
-if process.returncode > 0 or re.search('Failed', stderrdata):
-    raise Exception("""PreCluster step failed.\n
-Reason: \'%s\'""" % str(stderrdata))
-
-# 4) Run 'Cluster'
-project = os.path.dirname(os.path.realpath(__file__))
-joblist_filename = '%s.jobs' % project
-joblist = yaml.load(open(joblist_filename))
-
 if not os.path.exists('lsf'):
     os.mkdir('lsf')
 
-for job in joblist:
-    lsf = os.path.abspath(os.path.join('lsf', '%d.result' % job['jobid']))
-    call(['bsub', '-W', '8:00', '-o', lsf,
-         'jt', 'run', '--job', job['jobid']])
+# 2) Run 'PreCluster'
+print('\njt - Submitting PreCluster')
+lsf = os.path.abspath(os.path.join('lsf', '%.5d.precluster' % 1))
+call(['bsub', '-W', '8:00', '-o', lsf,
+     '-R', 'rusage[mem=4000,scratch=4000]',
+     'jt', 'run', '--job', '1'])
+
+
+
+# # 3) Run 'JTCluster'
+# print('\njt - Submit JTCluster')
+# project = os.path.dirname(os.path.realpath(__file__))
+# joblist_filename = '%s.jobs' % project
+# joblist = yaml.load(open(joblist_filename))
+
+# if not os.path.exists('lsf'):
+#     os.mkdir('lsf')
+
+# for job in joblist:
+#     lsf = os.path.abspath(os.path.join('lsf', '%.5d.jtcluster' % job['jobid']))
+#     call(['bsub', '-W', '8:00', '-o', lsf,
+#          '-R', 'rusage[mem=4000,scratch=4000]',
+#          'jt', 'run', '--job', job['jobid']])
