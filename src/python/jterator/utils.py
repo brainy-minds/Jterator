115394786a7d22c8c09e1f04d3c21c7c045159d5
Markus Herrmann <markus.herrmann@imls.uzh.ch>
2015-03-19 16:15:19 +0100

    Bug fix.

--- src/python/jterator/utils.py
+++ src/python/jterator/utils.py
@@ -18,13 +18,13 @@ def complete_yaml_path(input_path, variable):
     '''
     Complete module path, which can be provided in the pipeline descriptor
     file as full path, relative path or `variable` path (containing
-    the variable {libpath}).
+    the variable {key_name}).
     :input_path:   value of the key in the pipeline descriptor file
     :variable:     value of the key in the pipeline descriptor file
     '''
     # Replace the `variable` name with the actual value
     if variable and re.search(r'.*{.*}.*', input_path):
-        re_path = re.sub(r'{%s}' % variable, variable, input_path)
+        re_path = re.sub(r'{.*}', variable, input_path)
     else:
         re_path = input_path
     # Expand path starting with `~`
