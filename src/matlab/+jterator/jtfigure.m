d6db86844e7ef83a2523b96d1863a54583b77863
HACKERMD <markusdherrmann@gmail.com>
2015-04-08 15:01:56 +0200

    Remove APIs from old location

--- src/matlab/+jterator/jtfigure.m
+++
@@ -1,9 +0,0 @@
-function figure_url = jtfigure(fig, filename, fig_format)
-    if fig_format == 'pdf'
-        set(fig, 'PaperPosition', [0 0 7 7], 'PaperSize', [7 7]);
-        saveas(fig, sprintf('figures/%s', filename), 'pdf');
-    elseif fig_format == 'plotly'
-        figure_obj = fig2plotly(fig);    
-        figure_url = figure_obj.url;
-    end
-end
