64c723b5940a7852efafc89f7a85bf5d8578043e
HACKERMD <markusdherrmann@gmail.com>
2015-04-10 11:46:28 +0200

    Simplify module skeleton layout

--- skeleton/modules/myJuliaModule.jl
+++ skeleton/modules/myJuliaModule.jl
@@ -1,25 +1,23 @@
 importall jtapi
 
+
 mfilename = match(r"([^/]+)\.jl$", @__FILE__()).captures[1]
 
-###############################################################################
-## jterator input
+#########
+# input #
+#########
 
 @printf("jt - %s\n", mfilename)
 
-### read YAML from standard input
 handles_stream = readall(STDIN)
-
-### retrieve handles from .YAML files
 handles = gethandles(handles_stream)
-
-### read input arguments from .HDF5 files
 input_args = readinputargs(handles)
-
-### check whether input arguments are valid
 input_args = checkinputargs(input_args)
 
-###############################################################################
+
+##############
+# processing #
+##############
 
 # here comes your code
 
@@ -27,13 +25,9 @@ data = Dict()
 output_args = Dict()
 
 
-###############################################################################
-## jterator output
+##########
+# output #
+##########
 
-### write measurement data to HDF5
 writedata(handles, data)
-
-### write temporary pipeline data to HDF5
 writeoutputargs(handles, output_args)
-
-###############################################################################
--- skeleton/modules/myMatlabModule.m
+++ skeleton/modules/myMatlabModule.m
@@ -1,40 +1,33 @@
 import jtapi.*;
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% jterator input
+%%%%%%%%%
+% input %
+%%%%%%%%%
 
 fprintf(sprintf('jt - %s:\n', mfilename));
 
-%%% read standard input
-handles_stream = STDIN; % STDIN is provided by Mscript!
+cd(currentDirectory)  % 'currentDirectory' is provided by Mscript
 
-%%% change current working directory
-cd(currentDirectory)
-
-%%% retrieve handles from .YAML files
+handles_stream = STDIN;  % 'STDIN' is provided by Mscript
 handles = gethandles(handles_stream);
-
-%%% read input arguments from .HDF5 files
 input_args = readinputargs(handles);
-
-%%% check whether input arguments are valid
 input_args = checkinputargs(input_args);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%
+% processing %
+%%%%%%%%%%%%%%
 
 % here comes your code
 
 data = struct();
 output_args = struct();
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% jterator output
 
-%%% write measurement data to HDF5
+%%%%%%%%%%
+% output %
+%%%%%%%%%%
+
 writedata(handles, data)
-
-%%% write temporary pipeline data to HDF5
 writeoutputargs(handles, output_args)
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
